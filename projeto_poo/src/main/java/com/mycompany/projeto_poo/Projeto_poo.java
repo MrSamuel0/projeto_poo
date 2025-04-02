ae6ce9b7447b84be5b99c58bd90cf4a0a12eadee
Samuel Manhani Salles <samuca.manhani@gmail.com>
2025-04-01 21:54:11 -0300

    initial commit

---
+++ projeto_poo/src/main/java/com/mycompany/projeto_poo/Projeto_poo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_poo;
+
+/**
+ *
+ * @author samuel
+ */
+public class Projeto_poo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
